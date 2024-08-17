8e96feb507b2f13541e9f19c399cc9c00b5d6700
MaheshHub280 <maheshbarri2001@gmail.com>
2024-08-17 12:31:46 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,4 +10,13 @@ def newDeploy(jobname,ip,appname)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/tar get/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname} .war" 
 }
+def newselinum(repo,jobname)
+{
+   git "https://github.com/intelliqittrainings/${repo}"
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+def newDelivery(jobname,ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/tar get/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname} .war"
+}
    
