09c53c8a8dd4ff40f5460ae133da62104054bd18
MaheshHub280 <maheshbarri2001@gmail.com>
2024-08-17 10:38:07 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,21 @@
+def newDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}"
+}
+def newBuild()
+{
+  sh'mvn package'
+}
+def newDeployment(jobname,ip,contextpath)
+{
+   deploy "adapters: [tomcat9(credentialsId: '1289cb85-308b-4fb8-b93b-440eab01cfc4', path: '',${jobname}: 'http://${ip})], contextPath:${contextpath}, war: '**/*.war"
+}
+def newselinum(repo,jobname)
+{
+   git "https://github.com/intelliqittrainings/${repo}"
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+def newDelivery(jobname,ip,contextpath)
+{
+  deploy "adapters: [tomcat9(credentialsId: '1289cb85-308b-4fb8-b93b-440eab01cfc4', path: '',${jobname}: 'http://${ip})], contextPath:${contextpath}, war: '**/*.war"
+}
