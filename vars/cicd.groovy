44907dcd64edd48e80dbae51624cc8273a3d3ba1
MaheshHub280 <maheshbarri2001@gmail.com>
2024-08-18 15:04:06 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,18 +1,19 @@
 def newDownload(repo)
 {
-  git "https://github.com/intelliqittrainings/${repo}"
-}
-def newBuild()
-{
-  sh'mvn package'
-}
-def newDeploy(jobname,ip,appname)
-{
- sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
-}
-def newselinum(jobname)
-{
-   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+   git "https://github.com/intelliqittrainings/${repo}"
 }
 
-   
+def newBuild()
+{
+   sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,appname)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+
+def runSelenium(jobname)
+{
+sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
